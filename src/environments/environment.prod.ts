448308cfba0697d605e5d04ad0e0cc8b0fef874f
Marcos Turisco <turisco.tech@gmail.com>
2026-05-07 12:08:28 -0300

    feat: adicionando https

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://API_URL' // <-- A nossa isca para o sed!
+  apiUrl: 'https://API_URL' // <-- A nossa isca para o sed!
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost' // <-- Apenas o host base!
+  apiUrl: 'https://localhost' // <-- Apenas o host base!
 };
