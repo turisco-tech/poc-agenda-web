04c168cee5f5b44e2f870963d3a011006d3e8f2e
Marcos Turisco <turisco.tech@gmail.com>
2026-05-07 16:32:17 -0300

    fix: corrigindo path da API na cloud

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiPrincipalUrl: 'API_URL', // A esteira vai injetar https://agenda.turisco.com.br
-  apiDashboardUrl: 'API_URL'  // O mesmo domínio resolve as duas APIs via ALB!
+  apiPrincipalUrl: '', // Vazio mesmo!
+  apiDashboardUrl: ''  // Vazio mesmo!
 };
